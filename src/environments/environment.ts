57af140f45dd73d26b11124ad2a2f3d71615f6dc
Vadym-Bondarenko <67338686+Vadym-Bondarenko@users.noreply.github.com>
2021-10-28 19:41:38 +0200

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,30 +1,22 @@
 
 export const environment = {
   production: false,
-  urlFirebase: 'https://ecomuadmin-default-rtdb.europe-west1.firebasedatabase.app/',
-  urlLogin: 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyCNt8n3rtH6Pb0I9BRW_IhwmHJ8xkDHEpk',
-  urlGetUser: 'https://identitytoolkit.googleapis.com/v1/accounts:lookup?key=AIzaSyCNt8n3rtH6Pb0I9BRW_IhwmHJ8xkDHEpk',
-  urlFiles: 'http://localhost/sistemas-angular/marketplace/src/assets/img/',
-  adminFiles: 'http://localhost/sistemas-angular/marketplace/src/assets/img/index.php?key=AIzaSyCNt8n3rtH6Pb0I9BRW_IhwmHJ8xkDHEpk',
-  deleteFiles:'http://localhost/sistemas-angular/marketplace/src/assets/img/delete.php?key=AIzaSyCNt8n3rtH6Pb0I9BRW_IhwmHJ8xkDHEpk',
-  urlRefreshToken: 'https://securetoken.googleapis.com/v1/token?key=AIzaSyCNt8n3rtH6Pb0I9BRW_IhwmHJ8xkDHEpk',
-  urlEmail:'http://localhost/sistemas-angular/admin-angular/src/assets/email/index.php?key=AIzaSyCNt8n3rtH6Pb0I9BRW_IhwmHJ8xkDHEpk',
+  urlFirebase: '***',
+  urlLogin: '***',
+  urlGetUser: '***',
+  urlFiles: '***',
+  adminFiles: '***',
+  deleteFiles:'***',
+  urlRefreshToken: '***',
+  urlEmail:'***',
   domainMP:'http://localhost:4201/',
-  nameStore:'Digitalworld Us',
+  nameStore:'***',
 
-    authDomain: "ecomuadmin.firebaseapp.com",
-    projectId: "ecomuadmin",
-    storageBucket: "ecomuadmin.appspot.com",
-    messagingSenderId: "432963041940",
-    appId: "1:432963041940:web:349487e694101b2b769281"
+    authDomain: "***",
+    projectId: "***",
+    storageBucket: "***.appspot.com",
+    messagingSenderId: "***",
+    appId: "1:***:web:***"
 
 };
 
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
